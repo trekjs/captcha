517cae2c3e7aad5cb1c63af2943fbc3271bf4bea
Fangdun Cai <cfddream@gmail.com>
2017-02-20 18:15:32 +0800

    test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./build/Release/addon')
+try {
+  module.exports = require('./build/Release/addon')
+} catch (err) {
+  console.log(err)
+  module.exports = require('./build/Debug/addon')
+}
