ffe11862998226c37dba61a51c3a2f3391401606
Fangdun Cai <cfddream@gmail.com>
2017-03-16 14:28:50 +0800

    refactor with pure js

--- test/index.js
+++ test/index.js
@@ -1,9 +1,8 @@
-import fs from 'fs'
 import test from 'ava'
 import captcha from '..'
 
-test('should return an object and include token and buffer', t => {
-  const { token, buffer } = captcha()
+test('should return an object and include token and buffer', async t => {
+  const { token, buffer } = await captcha()
 
   t.is(token.length, 5)
   t.is(buffer.length, 17646)
